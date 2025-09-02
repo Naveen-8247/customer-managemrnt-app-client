c29e3e6d4cec6ccbfe5705713db26d10281502f2
Dyavar Naveen Kumar <dyavarnaveen8247@gmail.com>
2025-09-03 03:16:37 +0530

    Update index.js

--- src/components/CustomerDetailPage/index.js
+++ src/components/CustomerDetailPage/index.js
@@ -12,7 +12,7 @@ function CustomerDetailPage() {
 
   // Fetch customer details
   useEffect(() => {
-    axios.get("customer-managemnt-app-backend-production.up.railway.app/api/customers/" + id)
+    axios.get("https://customer-management-app-backend-production.up.railway.app/api/customers/" + id)
       .then((res) => setCustomer(res.data.data))
       .catch((err) => {
         setError(err.response?.data?.error || "Error fetching customer");
