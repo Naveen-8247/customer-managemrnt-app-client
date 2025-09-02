c23bc81e5c1f17db92c28f3300d1c243fac7e839
Dyavar Naveen Kumar <dyavarnaveen8247@gmail.com>
2025-09-03 03:15:58 +0530

    Update index.js

--- src/components/AddressForm/index.js
+++ src/components/AddressForm/index.js
@@ -14,7 +14,7 @@ function AddressForm({ customerId, onDone, onAddressAdded }) {
     const newAddress = { address_details: addressDetails, city, state, pin_code: pinCode };
 
     axios
-      .post(`customer-managemnt-app-backend-production.up.railway.app/api/customers/${customerId}/addresses`, newAddress)
+      .post(`https://customer-management-app-backend-production.up.railway.app/api/customers/${customerId}/addresses`, newAddress)
       .then(() => {
         onAddressAdded();
         onDone();
